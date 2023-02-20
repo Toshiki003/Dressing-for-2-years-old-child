7ff791b7b2d8c1970a10b66c60d1a9b69c61a84d
関目智史 <zou3road@gmail.com>
2023-02-20 23:05:17 +0900

    Add:requestスペックの軽微な修正とLintチェック修正 #22

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :require_login
 
   private
+
   def not_authenticated
     flash[:danger] = 'ログインしてください'
     redirect_to login_path
