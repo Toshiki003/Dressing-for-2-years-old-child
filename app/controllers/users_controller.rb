5aa8012bf16f16012c9432bb0965a5327dc5a22e
関目智史 <zou3road@gmail.com>
2023-02-20 16:23:21 +0900

    Fix:Lintチェックによる修正 #19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "OK"
+      redirect_to root_path, notice: 'OK'
     else
       render :new
     end
   end
 
-
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
