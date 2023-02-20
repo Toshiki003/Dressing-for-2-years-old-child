058517d493d978750cde829ae2142fc7d87ddc77
関目智史 <zou3road@gmail.com>
2023-02-20 23:02:21 +0900

    Add:usersコントローラのフラッシュメッセージをBootstrap適用に修正 #22

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: 'OK'
+      redirect_to root_path, success: "ユーザー登録が完了しました"
     else
       render :new
     end
