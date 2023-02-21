f8c00d9a0f400636cdf392552bde60cffbef60e8
関目智史 <zou3road@gmail.com>
2023-02-21 15:44:10 +0900

    Add:カラム名を修正 #25

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: { maximum: 255 }
-  validates :body, presence: true, length: { maximum: 65_535 }
+  validates :content, presence: true, length: { maximum: 65_535 }
   validates :embed_youtube, length: { maximum: 200 }
 end
