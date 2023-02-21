e41c8cfd6bebdb600da0cf972d25e98f1d3c34ba
関目智史 <zou3road@gmail.com>
2023-02-21 15:57:36 +0900

    Add:postコントローラのindex,new,createメソッド記述、routes追加 #25

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,31 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.includes(:user).order(created_at: :desc)
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, success: t('defaults.message.created', item: Post.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_created', item: Post.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def show
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :embed_youtube)
   end
 end
