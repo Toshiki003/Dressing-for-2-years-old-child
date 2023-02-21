a95acde89dc9296486c2bd1aa819b879c05cdc6e
関目智史 <zou3road@gmail.com>
2023-02-21 14:29:53 +0900

    Add:user_sessions_spec記述とログインマクロ修正 #20

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :system do
-  
+  let(:user) { create(:user) }
+
+  describe '通常のログインログアウト' do
+    context '正常系' do
+      it 'ログインができる' do
+        visit login_path
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード',	with: 'password'
+        click_button 'ログイン'
+        expect(page).to  have_content 'ログインしました'
+        # posts機能実装後コメントアウトを外す↓
+        # expect(current_path).to eq posts_path
+      end
+
+      it 'ログアウトができる' do
+        login_as(user)
+        click_link 'ログアウト'
+        expect(page).to  have_content 'ログアウトしました'
+      end
+    end
+
+    context '異常系' do
+      it 'メールアドレスが未入力の場合、ログインできない' do
+        visit login_path
+        fill_in 'メールアドレス', with: ''
+        fill_in 'パスワード',	with: 'password'
+        click_button 'ログイン'
+        expect(current_path).to eq login_path
+        expect(page).to  have_content 'ログインに失敗しました'
+      end
+
+      it 'パスワードが未入力の場合、ログインできない' do
+        visit login_path
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード',	with: ''
+        click_button 'ログイン'
+        expect(current_path).to eq login_path
+        expect(page).to  have_content 'ログインに失敗しました'
+      end
+    end
+  end
+
+  describe 'ゲストログイン' do
+    # 後で書きます
+  end
 end
