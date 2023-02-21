ee6fce24d802289f5c46f57cd4f7040c288c2e22
関目智史 <zou3road@gmail.com>
2023-02-21 15:32:19 +0900

    Add:UserモデルとPostモデルの関連付け、およびバリデーション定義 #25

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :body, presence: true, length: { maximum: 65_535 }
+  validates :embed_youtube, length: { maximum: 200 }
 end
