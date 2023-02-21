2548cab8d8fb6d6722942ece1054cdb2f738cb6d
関目智史 <zou3road@gmail.com>
2023-02-21 16:48:53 +0900

    Add:seed-fuデータ（user, post)を記述 #25

---
+++ db/fixtures/development/users.rb
@@ -0,0 +1,13 @@
+1.upto(10) do |user|
+  User.seed(
+    # 基本はidでseed-fuは投下データの同一性を判断する。idは省略可能なので、ユニークなemailで同一性判断をする場合の書き方になる。
+    # 参考:https://qiita.com/punkshiraishi/items/18c80475fcd50d708dc4
+  :email,
+  {
+  name: "test_#{user}",
+  email: "test#{user}@example.com",
+  crypted_password: "password#{user}",
+  salt: "password#{user}"
+  }
+  )
+end
