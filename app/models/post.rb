56c4377776fccab619ec65a333063aa9774c8a3d
関目智史 <zou3road@gmail.com>
2023-02-22 23:50:49 +0900

    Add:youtube埋め込みをビューで表示 #40

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,15 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 65_535 }
   validates :embed_youtube, length: { maximum: 200 }
+
+  def split_id_from_youtube_url
+    # YoutubeならIDのみ抽出
+    if self.embed_youtube?
+      if self.embed_youtube.include?('youtu.be')
+        embed_youtube.split('/').last
+      elsif self.embed_youtube.include?('youtube.com')
+        embed_youtube.split('=').last
+      end
+    end
+  end
 end
