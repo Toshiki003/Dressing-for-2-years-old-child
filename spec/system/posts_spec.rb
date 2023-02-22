6c3724b182b09587fdbcc577b59e32f908117473
関目智史 <zou3road@gmail.com>
2023-02-22 22:48:34 +0900

    Fix:post_specのログイン後記述の削除 #38

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -38,22 +38,6 @@ RSpec.describe "Posts", type: :system do
     end
   end
          
-  describe 'ログイン後' do
-    before { login_as(user) }
-
-    describe '投稿の新規作成' do
-      context '投稿の入力値が正常' do
-        it '投稿の新規作成が成功する' do
-          visit new_post_path
-          fill_in 'タイトル', with: 'title'
-          fill_in '内容', with: 'content'
-          fill_in 'YouTube URL(任意)', with: 'https://www.youtube.com/watch?v=BMis-npzi04'
-          click_button '登録する'
-          expect(page).to have_content ["t('defaults.message.created', item: Post.model_name.human)を作成しました"]
-          expect(current_path).to eq posts_path
-        end
-      end
-    end
-  end
+  
       
 end
