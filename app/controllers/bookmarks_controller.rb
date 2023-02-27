5aad35939f02f060232f1409a54bfefdd57b084a
関目智史 <zou3road@gmail.com>
2023-02-27 23:54:11 +0900

    Fix:オープンリダイレクトの修正 #45

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,12 +2,12 @@ class BookmarksController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     current_user.bookmark(post)
-    redirect_back fallback_location: posts_path, success: t('.success')
+    redirect_to posts_path, success: t('.success')
   end
 
   def destroy
     post = current_user.bookmarks.find(params[:id]).post
     current_user.unbookmark(post)
-    redirect_back fallback_location: posts_path, success: t('.success')
+    redirect_to posts_path, success: t('.success')
   end
 end
