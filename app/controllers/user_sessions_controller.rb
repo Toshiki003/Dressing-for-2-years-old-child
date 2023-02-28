cd0a0c019b8004eb85c54d557c1b8a27de12955f
関目智史 <zou3road@gmail.com>
2023-02-28 22:37:09 +0900

    Add:Usersテーブルにroleカラムを追加,ゲストログイン機能修正 #55

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -17,11 +17,12 @@ class UserSessionsController < ApplicationController
   
   def guest_login
     @guest_user = User.create(
-      name: 'ゲストユーザー',
+      name: 'ゲスト' + Time.zone.now.to_i.to_s + 'さん',
       email: SecureRandom.alphanumeric(10) + "@email.com",
       password: 'password',
       password_confirmation: 'password',
-      avatar: User.avatars.keys.sample
+      avatar: User.avatars.keys.sample,
+      role: 2
     )
     auto_login(@guest_user)
     redirect_to posts_path, success: t('.success')
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_133225) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_130055) do
   create_table "bookmarks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_133225) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "avatar", default: 0, null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
