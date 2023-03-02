727f5420478e1a5c5aa382d44d865c66cc84fcfb
関目智史 <zou3road@gmail.com>
2023-03-02 17:12:28 +0900

    db/fixturesを追加

---
+++ db/fixtures/production/001_users.rb
@@ -0,0 +1,19 @@
+
+
+User.seed(:email) do |s|
+  s.name = "toshiki003"
+  s.email = "tototo@example.com"
+  s.password = "password"
+  s.password_confirmation = "password"
+  s.role = 0
+  s.avatar = 8
+end
+
+User.seed(:email) do |s|
+  s.name = "きよし"
+  s.email = "kiyo@example.com"
+  s.password = "password"
+  s.password_confirmation = "password"
+  s.role = 0
+  s.avatar = 7
+end
---
+++ db/fixtures/production/002_categories.rb
@@ -0,0 +1,8 @@
+Category.seed(:id, [
+  { id: 1, name: "お風呂" },
+  { id: 2, name: "歯磨き" },
+  { id: 3, name: "お片付け" },
+  { id: 4, name: "食事" },
+  { id: 5, name: "着替え" },
+  { id: 6, name: "おでかけ" },
+])
