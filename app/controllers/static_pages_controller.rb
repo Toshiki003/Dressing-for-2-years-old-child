2c31ea3a74d5252eba0d42609719c5235b79f4f7
関目智史 <zou3road@gmail.com>
2023-03-12 20:13:48 +0900

    Add:termsページにフッターからアクセスできるよう修正 #89

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top privacy_policy]
+  skip_before_action :require_login, only: %i[top privacy_policy terms]
   def top
     @new_arrivals = Post.new_arrivals
   end
 
   def privacy_policy; end
+
+  def terms; end
 end
