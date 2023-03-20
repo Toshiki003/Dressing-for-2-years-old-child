0361355a5fbc579df8b2b4b5a96fbba63aa10e25
関目智史 <zou3road@gmail.com>
2023-03-20 23:50:05 +0900

    Modify:既存のbookmarkのturbo修正 #47

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,12 @@
 class BookmarksController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    current_user.bookmark(post)
-    redirect_to posts_path, success: t('.success')
+    @post = Post.find(params[:post_id])
+    current_user.bookmark(@post)
+    
   end
 
   def destroy
-    post = current_user.bookmarks.find(params[:id]).post
-    current_user.unbookmark(post)
-    redirect_to posts_path, success: t('.success'), status: :see_other 
+    @post = current_user.bookmarks.find(params[:id]).post
+    current_user.unbookmark(@post)
   end
 end
