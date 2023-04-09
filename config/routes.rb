3dee803f46aaf0090f7f9f79345c740e713bd4c7
関目智史 <zou3road@gmail.com>
2023-04-09 15:39:49 +0900

    Add:画像、ルーティングなどを追加 #126

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#top'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   get 'terms', to: 'static_pages#terms'
+  get 'about', to: 'static_pages#about'
 
   resources :users, only: %i[new create]
   get 'login', to: 'user_sessions#new'
